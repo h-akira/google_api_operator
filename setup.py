46b4909f731cd1f831304746cfa3e076a187bab1
h-akira <79441679+h-akira@users.noreply.github.com>
2023-05-08 04:18:48 +0900

    test

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ from setuptools import setup, find_packages
 def requirements_from_file(file_name):
   return open(file_name).read().splitlines()
 
-setup(name='google_api_operator',version='1.0.0',packages=find_packages(), install_requires=requirements_from_file('requirements.txt'))
+# setup(name='google_api_operator',version='1.0.0',packages=find_packages(), install_requires=requirements_from_file('requirements.txt'))
+setup(name='google_api_operator',version='1.0.0',packages=find_packages())
